51cb131b1b0e090e6757a318de91dad4b85644c9
soapcookie <sgua0720@gmail.com>
2024-07-10 15:34:43 +0900

    first

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IndividualRegisterView, BusinessRegisterView,IndividualLoginView, BusinessLoginView
+from .views import IndividualRegisterView, BusinessRegisterView,IndividualLoginView, BusinessLoginView, UserProfileView
 
 urlpatterns = [
     path('register/individual/', IndividualRegisterView.as_view(), name='individual-register'),
     path('register/business/', BusinessRegisterView.as_view(), name='business-register'),
     path('login/individual/', IndividualLoginView.as_view(), name='individual-login'),
     path('login/business/', BusinessLoginView.as_view(), name='business-login'),
+    path('profile/', UserProfileView.as_view(), name='user-profile'),
 ]
