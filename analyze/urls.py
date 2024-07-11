b6df198aa0b82aa80a355b4bda038245f50bfbed
unknown <c912t117@gmail.com>
2024-07-11 16:40:01 +0900

    add dalle_result

--- analyze/urls.py
+++ analyze/urls.py
@@ -1,8 +1,9 @@
 # analyze/urls.py
 from django.urls import path
-from .views import upload_image, result_image
+from .views import upload_image, dalle_result, create_order  # create_order 추가
 
 urlpatterns = [
     path('upload/', upload_image, name='upload_image'),
-    path('result/', result_image, name='result_image'),
+    path('dalle_result/', dalle_result, name='dalle_result'),
+    path('create_order/', create_order, name='create_order'),  # create_order 경로 추가
 ]
