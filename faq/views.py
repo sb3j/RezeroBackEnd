080cdb56dcc2675ec73880985d6d57cbca498e5d
unknown <c912t117@gmail.com>
2024-07-15 14:04:01 +0900

    fix:faq 기능 추가

--- faq/views.py
+++ faq/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, filters
 from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.exceptions import ParseError
 from django.db.models import Q
+from django.shortcuts import render
 from .models import FAQ
 from .serializers import FAQSerializer
 
@@ -10,6 +11,7 @@ class FAQListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = FAQSerializer
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['category']
+    template_name = 'faq/faq_list.html'  # 추가된 부분
 
     def get_permissions(self):
         if self.request.method == 'GET':
@@ -32,6 +34,14 @@ class FAQListCreateAPIView(generics.ListCreateAPIView):
                 raise ParseError(detail="Invalid search_type parameter. Valid values are 'question', 'answer', or 'both'.")
         return queryset
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        context = {
+            'object_list': queryset,
+        }
+        return render(request, self.template_name, context)
+
+
 class FAQRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = FAQ.objects.all()
     serializer_class = FAQSerializer
@@ -40,3 +50,15 @@ class FAQRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
         if self.request.method == 'GET':
             return [AllowAny()]
         return [IsAdminUser()]
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        previous_faq = FAQ.objects.filter(id__lt=instance.id).order_by('-id').first()
+        next_faq = FAQ.objects.filter(id__gt=instance.id).order_by('id').first()
+        context = {
+            'faq': instance,
+            'previous_faq': previous_faq,
+            'next_faq': next_faq,
+        }
+        return render(request, 'faq/faq_detail.html', context)
+
