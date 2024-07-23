aa0440e3ea2224f4df13b74a951c469abacac8a6
soapcookie <sgua0720@gmail.com>
2024-07-23 14:47:56 +0900

    fix:token

--- remage/settings.py
+++ remage/settings.py
@@ -78,8 +78,8 @@ REST_FRAMEWORK = {
  
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
