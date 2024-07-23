0fd2ab0a010588d5fc2fd19ff74a7c6e52bbb53d
Maba <gigig9647@gmail.com>
2024-07-23 01:30:26 -0300

    'commit'

--- remage/settings.py
+++ remage/settings.py
@@ -131,9 +131,9 @@ DATABASES = {
 }
 
 SIMPLE_JWT = {
-    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     # 'ACCESS_TOKEN_LIFETIME': timedelta(seconds=5),
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
